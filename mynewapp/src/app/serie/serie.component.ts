b5f8b5f032fad36850c57301f7f3c42710e2f0ab
d.zamorac <d.zamorac@uniandes.edu.co>
2022-10-31 19:36:03 -0500

    Taller terminado con barra de decoracion

--- mynewapp/src/app/serie/serie.component.ts
+++ mynewapp/src/app/serie/serie.component.ts
@@ -11,15 +11,27 @@ import { SerieService } from './serie.service';
 export class SerieComponent implements OnInit {
 
   series: Array<Serie> = [];
+  promedio: number=0;
   constructor(private serieService: SerieService) { }
 
   getSeries() {
     this.serieService.getSeries().subscribe(series => {
       this.series = series;
+      this.getAverage(series)
     });
   }
 
+  getAverage(series:Serie[]){
+    this.promedio=0
+    let totalSeasons: number = 0;
+    let numberOfSeries: number =0;
+    series.forEach((serie) => {
+      totalSeasons = totalSeasons + serie.seasons
+      numberOfSeries=numberOfSeries+1
+      });
+      this.promedio =totalSeasons/numberOfSeries;
 
+  }
   ngOnInit() {
     this.getSeries();
   }
